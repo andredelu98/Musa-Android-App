45e61286e2f1f207f595aaf2c009dc7b8adc181d
andredelu98 <andrea.deluca1108@gmail.com>
2024-01-22 19:47:34 +0100

    Modifica della bottom bar (va risolto il problema del colore)

--- app/src/main/java/it/polito/musaapp/Backend/AppNavigation.kt
+++ app/src/main/java/it/polito/musaapp/Backend/AppNavigation.kt
@@ -3,24 +3,34 @@ package it.polito.musaapp.Backend
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -50,7 +60,9 @@ fun AppNavigation(vm: MusaViewModel, applicationContext: Context) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
-
+    val navigationBarColors = NavigationBarItemDefaults.colors(
+        indicatorColor = MaterialTheme.colorScheme.surface
+    )
     Scaffold(
         bottomBar = {
             if (navBackStackEntry?.destination?.route != (Screens.WelcomePage.name)
@@ -59,8 +71,7 @@ fun AppNavigation(vm: MusaViewModel, applicationContext: Context) {
                     modifier = Modifier
                         .background(MaterialTheme.colorScheme.background)
                         .clip(shape = RoundedCornerShape(15.dp, 20.dp, 0.dp, 0.dp)),
-                    containerColor = MaterialTheme.colorScheme.surface,
-                    contentColor = MaterialTheme.colorScheme.onSurface,
+                    containerColor = MaterialTheme.colorScheme.surface                  //COLORE NON MOSTRA QUESTO
                 ) {
                     val navBackStackEntry by navController.currentBackStackEntryAsState()
                     val currentDestination = navBackStackEntry?.destination
@@ -68,7 +79,9 @@ fun AppNavigation(vm: MusaViewModel, applicationContext: Context) {
                     listOfNavItems.forEach { navItem ->
                         NavigationBarItem(
                             selected = currentDestination?.hierarchy?.any { it.route == navItem.route } == true,
+                            colors = navigationBarColors,
                             onClick = {
+                                listOfNavItems.forEach { it.selected = it == navItem }
                                 navController.navigate(navItem.route) {
                                     popUpTo(navController.graph.findStartDestination().id) {
                                         saveState = true
@@ -78,17 +91,24 @@ fun AppNavigation(vm: MusaViewModel, applicationContext: Context) {
                                 }
                             },
                             icon = {
-                                Icon(
-                                    imageVector = navItem.icon,
-                                    contentDescription = null,
-                                    tint = Color.White
-                                )
-                            },
-                            label = {
-                                Text(
-                                    text = navItem.label,
-                                    color = Color.White
-                                )
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .padding(0.dp, 5.dp),
+                                    verticalArrangement = Arrangement.SpaceEvenly,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Image(
+                                        painter = painterResource(id = if (navItem.selected) navItem.iconId_selected else navItem.iconId_unselected),
+                                        contentDescription = null,
+                                        modifier = Modifier.size(40.dp) // Puoi regolare la dimensione secondo le tue esigenze
+                                    )
+                                    Text(
+                                        text = navItem.label,
+                                        fontSize = 11.sp,
+                                        color = MaterialTheme.colorScheme.background
+                                    )
+                                }
                             },
                             modifier = Modifier.fillMaxHeight()
                         )
--- app/src/main/java/it/polito/musaapp/Backend/NavItems.kt
+++ app/src/main/java/it/polito/musaapp/Backend/NavItems.kt
@@ -6,30 +6,37 @@ import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.ui.graphics.vector.ImageVector
+import it.polito.musaapp.R
 import it.polito.musaapp.Screens
 
 
 data class NavItem(
     val label: String,
-    val icon: ImageVector,
+    val iconId_unselected: Int,
+    val iconId_selected: Int,
+    var selected: Boolean = false,
     val route: String,
 )
 
 val listOfNavItems = listOf(
     NavItem(
         label = "Progetti",
-        icon = Icons.Filled.List,
+        iconId_unselected = R.drawable.progetti,
+        iconId_selected = R.drawable.progetti_pieni,
         route = Screens.ProjectPage.name,
     ),
     NavItem(
-        label = "Esercizi",
-        icon = Icons.Filled.Home,
+        label = "Home",
+        iconId_unselected = R.drawable.home,
+        iconId_selected = R.drawable.home_piena,
+        selected = true,
         route = Screens.HelpPage.name,
     ),
 
     NavItem(
         label = "Profilo",
-        icon = Icons.Filled.Person,
+        iconId_unselected = R.drawable.profilo,
+        iconId_selected = R.drawable.profilo_pieno,
         route = Screens.ProfilePage.name,
     )
 )
